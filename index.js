4c051d9b221180f1c13d4bf740f5ba888b2030c7
Jared McMahan <jaredm788@gmail.com>
2022-09-14 19:55:13 -0600

    commit tree 4599ed47dbf1e462846a041da73dfe4b90bb0ba8

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let n = word.length
+  console.log("hello")
+  for(let i = 0; i < n; i++,n--)
+  if (word[i] != word[n - 1])
+  return false
+  return true
+  
 }
 
 /* 
-  Add your pseudocode here
+  //reate a function that will take in one argument as a string,
+  //loop/iterate over each string element and return true if the string is a palindrom
+  otherwise 
+  return false if string is not a palindrome 
+  return true if string is palindrome
 */
 
 /*
-  Add written explanation of your solution here
+  I wrote a function that took in the argument "word". then I assigned each word length to the the variable n.
+  After that, I created a for loop that ran through each word. In the loop I assigned 0 to the variable i. if i is less than n(the word length, then )
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("expecting: to return string")
+  // console.log()
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -23,3 +39,11 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+// initialize empty array called result
+
+// iterate over each item in the input array:
+//   if element is a number:
+//     push item onto result
+
+// return result
